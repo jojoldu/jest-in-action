3970fa1fba6fe7d6bb2b80d0e3e711bd05354caf
jojoldu <jojoldu@gmail.com>
2021-12-28 22:44:15 +0900

    예제 코드 추가

---
+++ posts/clear-mock/README.md
@@ -0,0 +1,5 @@
+# Jest 에서 Mock/Spy/Stub 초기화방법 비교
+
+
+
+
---
+++ test/clear-mock/test1.ts
@@ -0,0 +1,17 @@
+const mockFn = jest.fn();
+
+function fnUnderTest(args1) {
+    mockFn(args1);
+}
+
+test('Testing once', () => {
+    fnUnderTest('first-call');
+    expect(mockFn).toHaveBeenCalledWith('first-call');
+    expect(mockFn).toHaveBeenCalledTimes(1);
+});
+
+test('Testing twice', () => {
+    fnUnderTest('second-call');
+    expect(mockFn).toHaveBeenCalledWith('second-call');
+    expect(mockFn).toHaveBeenCalledTimes(1);
+});
